d98add06bc436e2a0772cdd56cfca13650511858
Tabitha Becker <tabithabecker81@gmail.com>
2025-11-09 14:13:48 -0600

    Update hw3Script.js

--- hw3Script.js
+++ hw3Script.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', (event) =>
     getStateList();
   })
 
+
 // Adding in script to get current date and return it. Originally found on W3schools.com then modified
 function getDate() 
   {
@@ -62,6 +63,8 @@ function getFooter()
 function validateFname() 
   {
     const fnameValid = document.getElementById("fnameValidate").value;
+    const errorFlag = 0;
+
     if(fnameValid.length < 1) {
       document.getElementById("fnameErrorMsg").innerHTML = "Error: First name must be 1 or more characters";
       fnameErrorMsg.style.display = "block";
@@ -78,12 +81,15 @@ function validateFname()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
   //Validating the middle name. The error only displays when an invalid character is added. Otherwise no error will appear.
   // Referencing https://profjake.w3spaces.com/MIS3371/homework3.html 
 function validateMidname() 
   {
     const midnameValid = document.getElementById("midnameValidate").value;
+    const errorFlag = 0;
+
     if(midnameValid.length < 1 ) {
       document.getElementById("midnameErrorMsg").innerHTML = "";
       midnameErrorMsg.style.display = "none";
@@ -100,11 +106,14 @@ function validateMidname()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Validating last name 
 function validateLname() 
   {
     const lnameValid = document.getElementById("lnameValidate").value;
+    const errorFlag = 0;
+
     if(lnameValid.length < 1) {
       document.getElementById("lnameErrorMsg").innerHTML = "Error: Last name must be 1 or more characters";
       lnameErrorMsg.style.display = "block";
@@ -121,11 +130,14 @@ function validateLname()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
  //Validating SSN. The regex pattern makes the dashes optional.
 function validateSsn() 
   {
     const ssnValid = document.getElementById("ssnValidate").value;
+    const errorFlag = 0;
+
     if(ssnValid.length < 9) {
       document.getElementById("ssnErrorMsg").innerHTML = "Error: SSN must be 9 characters";
       ssnErrorMsg.style.display = "block";
@@ -142,12 +154,27 @@ function validateSsn()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
  //Validating Date of Birth.
 function validateDob() 
   {
     //pulls the current date from the function getDate()
     const currentDate = new Date(getDate());
+    const dobInput = document.getElementById("dobValidate").value;
+    const dateErrorMsg = document.getElementById("dateErrorMsg");
+    const errorFlag = 0;
+   
+    //Checking if the user did not enter a date
+    if (!dobInput) {
+      dateErrorMsg.innerHTML = "Error: Please enter a date of birth";
+      dateErrorMsg.style.display = "block";
+      console.error("Error: Please enter a date of birth")
+      errorFlag = 1;
+      // return will stop the function early if there is no date entered
+      return;  
+    }
+
     //bringing in the date the user enters
     const userDate = new Date(document.getElementById("dobValidate").value);
     //Assigning th minimum date to be 120 years prior to currentDate
@@ -155,24 +182,26 @@ function validateDob()
     minDate.setFullYear(minDate.getFullYear() - 120);
 
     //Checking if the user's date is greater than the current date to make sure the date is not in the future.
+    
     if (userDate > currentDate) {
-        document.getElementById("dateErrorMsg").innerHTML = "Error invalid Date: (Date cannot be in the future)";
+        dateErrorMsg.innerHTML = "Error invalid Date: (Date cannot be in the future)";
         dateErrorMsg.style.display = "block";
         console.error("Error invalid Date: (Date cannot be in the future)")
         errorFlag = 1;
     }
     //Checking if the user's date is less than the minimum date to make sure the date is not more than 120 years from current date.
     else if (userDate <= minDate) {
-        document.getElementById("dateErrorMsg").innerHTML = "Error invalid Date: (Date must be within 120 years from today)";
+        dateErrorMsg.innerHTML = "Error invalid Date: (Date must be within 120 years from today)";
         dateErrorMsg.style.display = "block";
         console.error("Error invalid Date: (Date must be within 120 years from today)")
         errorFlag = 1;
     }
     //If date is within range the else statement clears the error message.
     else {
-        document.getElementById("dateErrorMsg").innerHTML = "";
+        dateErrorMsg.innerHTML = "";
         dateErrorMsg.style.display = "none";
     }
+    return errorFlag
   }
 
 /* Followed tutorial for phone number formatting in javascript at this link
@@ -203,6 +232,8 @@ function validatePhone()
   {
     //Retrieving the entered phone number's value
     const phoneValid = document.getElementById("phoneValidate").value;
+    const errorFlag = 0;
+
     //Checking if the phone number length is at least 10 characters.
     if(phoneValid.length < 10) {
       document.getElementById("phoneErrorMsg").innerHTML = "Error: Phone number must be 10 to 12 characters";
@@ -221,12 +252,14 @@ function validatePhone()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Verifying email address to make sure it is at least 5 characters long, 
 // there is an @ symbol and it must have a dot and at least 2 characters after the dot. 
 function validateEmail() 
   {
     const emailValid = document.getElementById("emailValidate").value;
+    const errorFlag = 0;
     if(emailValid.length < 5) {
       document.getElementById("emailErrorMsg").innerHTML = "Error: email address must be 5 or more characters";
       emailErrorMsg.style.display = "block";
@@ -243,11 +276,14 @@ function validateEmail()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Validating the address to make sure it only includes numbers, letters, comma, period, apostrophe, hyphen or a hashtag. 
 function validateAddress() 
   {
     const addressValid = document.getElementById("addressValidate").value;
+    const errorFlag = 0;
+
     if(addressValid.length < 2) {
       document.getElementById("addressErrorMsg").innerHTML = "Error: address must be 2 or more characters";
       addressErrorMsg.style.display = "block";
@@ -264,11 +300,14 @@ function validateAddress()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Address 2 is optional but if entered, validating the address to make sure it only includes numbers, letters, comma, period, apostrophe, hyphen or a hashtag. 
 function validateAddress2() 
   {
     const address2Valid = document.getElementById("address2Validate").value;
+    const errorFlag = 0;
+
     if(address2Valid.length < 1) {
       document.getElementById("address2ErrorMsg").innerHTML = "";
       address2ErrorMsg.style.display = "none";
@@ -284,11 +323,14 @@ function validateAddress2()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Validating the City
 function validateCity() 
   {
     const cityValid = document.getElementById("cityValidate").value;
+    const errorFlag = 0;
+
     if(cityValid.length < 2) {
       document.getElementById("cityErrorMsg").innerHTML = "Error: City must be 2 or more characters";
       cityErrorMsg.style.display = "block";
@@ -305,11 +347,14 @@ function validateCity()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Validating the zip code. Must be at least 5 digits but can optionally choose a - and four more digits.
 function validateZip() 
   {
     const zipValid = document.getElementById("zipValidate").value;
+    const errorFlag = 0;
+
     if(zipValid.length < 5) {
       document.getElementById("zipErrorMsg").innerHTML = "Error: Zip Code at least 5 characters";
       zipErrorMsg.style.display = "block";
@@ -326,11 +371,14 @@ function validateZip()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 //Validating Emergency Contact information. All ec info is optional so not error will show unless something is entered incorrectly.
 function validateEcFname() 
   {
     const ecFnameValid = document.getElementById("ecFnameValidate").value;
+    const errorFlag = 0;
+
     if(ecFnameValid.length < 1) {
       document.getElementById("ecFnameErrorMsg").innerHTML = "";
       ecFnameErrorMsg.style.display = "none";
@@ -346,11 +394,14 @@ function validateEcFname()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 
 function validateEcLname() 
   {
     const ecLnameValid = document.getElementById("ecLnameValidate").value;
+    const errorFlag = 0;
+
     if(ecLnameValid.length < 1) {
       document.getElementById("ecLnameErrorMsg").innerHTML = "";
       ecLnameErrorMsg.style.display = "none";
@@ -366,6 +417,7 @@ function validateEcLname()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 
 /* Followed tutorial for phone number formatting in javascript at this link
@@ -379,6 +431,8 @@ function ecPhoneNumFormatter() {
 function validateEcPhone() 
   {
     const ecPhoneValid = document.getElementById("ecPhoneValidate").value;
+    const errorFlag = 0;
+
     if(ecPhoneValid.length < 1) {
       document.getElementById("ecPhoneErrorMsg").innerHTML = "";
       ecPhoneErrorMsg.style.display = "none";
@@ -394,11 +448,14 @@ function validateEcPhone()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 
 function validateEcEmail() 
   {
     const ecEmailValid = document.getElementById("ecEmailValidate").value;
+    const errorFlag = 0;
+
     if(ecEmailValid.length < 1) {
       document.getElementById("ecEmailErrorMsg").innerHTML = "";
       ecEmailErrorMsg.style.display = "none";
@@ -414,6 +471,7 @@ function validateEcEmail()
         errorFlag = 1;
       }
     }
+    return errorFlag
   }
 
 //Referenced https://www.w3schools.com/howto/howto_js_rangeslider.asp to bring in the slider value
@@ -433,14 +491,15 @@ function validateUserId()
   {
     let userIdValid = document.getElementById("userIdValidate").value;
     let errorMsg = document.getElementById("userIdErrorMsg")
-    let errorFlag = 0;
+    const errorFlag = 0;
 
-    if(userIdValid.length < 5) {
-      errorMsg.innerHTML = "Error: Username is required and must be 5 or more characters. It cannot begin with a number";
+    if(userIdValid.length < 5 || /^[0-9]/.test(userIdValid)) {
+      errorMsg.innerHTML = "Error: Username is required and must be 5 or more characters and it cannot begin with a number";
       errorMsg.style.display = "block";
       errorFlag = 1;
       }
-    else if (!userIdValid.match(/^[a-zA-Z][a-zA-Z0-9_-]*$/)) {
+
+    else if (!/^[a-zA-Z][a-zA-Z0-9_-]*$/.test(userIdValid)) {
       errorMsg.innerHTML = "Username Error: (Invalid characters- only letters, numbers, underscore or dashes allowed. No spaces.)";
       userIdErrorMsg.style.display = "block";
       errorFlag = 1;
@@ -464,6 +523,7 @@ function validatePassword()
     let specialCharReq = document.getElementById("spCharReq");
     let passLength = document.getElementById("lengthReq");
     let passMatchReq = document.getElementById("passMatch")
+
 
     passwordToValidate.onkeyup = function() {
 
@@ -571,3 +631,4 @@ function reviewData() {
 
     }
 }
+
